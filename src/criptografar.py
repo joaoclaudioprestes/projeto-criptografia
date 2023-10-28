87faddef9d41f4f77f4e993ba30362e7eeb3c433
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2023-10-27 22:04:08 -0300

    Update criptografar.py

--- src/criptografar.py
+++ src/criptografar.py
@@ -1,7 +1,7 @@
 from src import rotacionar
 from src import config
 
-def criptografar(mensagem, key):
+def cesar_encript(mensagem, key):
     text_cripto = ''
     for i in mensagem: 
         if i in config.alfabeto:
@@ -11,6 +11,30 @@ def criptografar(mensagem, key):
             text_cripto += config.alfabeto[new_index]
         else:
             text_cripto += i 
+    
+    return text_cripto
+
+def cesar_encript2(cesar, key):
+    text_cripto = ''
+    for i in cesar:
+        if i in config.alphabet:
+            index = config.alphabet.find(i)
+            new_index = (index + key) % len(config.alphabet)
+            rotacionar.novo_index(new_index)
+            text_cripto += config.alphabet[new_index]
+        else:
+            text_cripto += i
 
     return text_cripto
-    
+
+def palavra_hex(cesar2):
+    palavra_hex = ''.join(f'{ord(letra):02x}' for letra in cesar2)
+
+    return palavra_hex
+
+def criptografar(mensagem, key):
+    cesar = cesar_encript(mensagem, key)
+    cesar2 = cesar_encript2(cesar, key)
+    # binario = palavra_hex(cesar2)
+
+    return cesar2
